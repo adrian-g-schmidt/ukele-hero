cd31b496951530c3b29899ce80b14dcc8863b712
agschmid <agschmid@andrew.cmu.edu>
2024-05-28 12:24:36 -0400

    brown out

--- ukele-controller/ukele-controller.ino
+++ ukele-controller/ukele-controller.ino
@@ -1,32 +1,37 @@
-/**
- * This example turns the ESP32 into a Bluetooth LE keyboard that writes the words, presses Enter, presses a media key and then Ctrl+Alt+Delete
- */
+// Load the libraries
 #include <Arduino.h>
 #include <BleGamepad.h>
+#include "soc/soc.h"
+#include "soc/rtc_cntl_reg.h"
 
+// Make a new gamepad for bluetooth
 BleGamepad bleGamepad("Ukele", "Adrian", 100);
-const int buttonPinCount = 7;
-const int buttonPins[] = {15, 2, 0, 4, 16, 17, 5};
-const int buttonValues[] = {BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4, BUTTON_5, BUTTON_6, BUTTON_7};
-const int lrPin = 36; // Left/Right
-const int udPin = 39; // Up/Down
+
+//Button variables - order doesn't matter as I remap in the game. 
+const int buttonPinCount = 9;
+const int buttonPins[] = {15, 2, 0, 4, 16, 17, 5, 18, 19};
+const int buttonValues[] = {BUTTON_1, BUTTON_2, BUTTON_3, BUTTON_4, BUTTON_5, BUTTON_6, BUTTON_7, BUTTON_8, BUTTON_9,};
+const int lrPin = 36;
+const int udPin = 39; 
 
 void setup()
 {
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // Set brownout voltage to 2.6V, so the arduino consistently runs off the battery
     Serial.begin(115200);
-    Serial.println("Starting BLE work!");
     bleGamepad.begin();
     for (int i = 0; i < buttonPinCount; i++) {
-        pinMode(buttonPins[i], INPUT_PULLUP);
+        pinMode(buttonPins[i], INPUT_PULLUP); 
     }
     pinMode(lrPin, INPUT);
     pinMode(udPin, INPUT);
 }
 
+
 void loop()
 {
     if (bleGamepad.isConnected())
     {
+      // Loop over all the buttons and send whether they are being pressed or released
         for (int i = 0; i < buttonPinCount; i++) {
             if (digitalRead(buttonPins[i]) == LOW) {
                 bleGamepad.press(buttonValues[i]);
